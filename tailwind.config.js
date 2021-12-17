5965a3b801767a20a7617ffdd0399a5b6658eb20
takusuzu601 <takusuzu601@gmail.com>
2021-12-17 11:03:26 +0900

    tailwind css 追加

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            //オリジナルカラーを拡張
+            colors: {
+                trueGray:colors.trueGray,
+                orange:colors.orange,
+                greenLime:colors.lime,
+              }
+        },
+    },
+
+   
+};
